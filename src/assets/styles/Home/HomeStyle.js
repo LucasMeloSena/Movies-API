310b08258549542024baebc897f839fc6c1ec6d4
LucasMeloSena <lucasmelosena@gmail.com>
2023-09-02 12:18:42 -0300

    update

--- src/assets/styles/Home/HomeStyle.js
+++ src/assets/styles/Home/HomeStyle.js
@@ -31,6 +31,7 @@ const HomeStyle = {
     font-size: 14px;
     height: 25px;
     width: 350px;
+    margin-bottom: 100px;
     &:focus {
         outline: none;
     }
@@ -42,6 +43,55 @@ const HomeStyle = {
     &:hover {
         cursor: pointer;
     }
+    `,
+
+    ContainerResponse : styled.div `
+        width: 50vw;
+        height: 250px;
+        background-color: #323d52;
+        border-radius: 5px;
+        box-shadow: 0 0 8px rgba(255, 255, 255, 1);
+        margin-bottom: 5vh;
+        display: flex;
+        flex-direction: row;
+    `,
+
+    ImgFilme : styled.img `
+    height: 250px;
+    width: 180px;
+    `,
+
+    ContainerMovieInfo : styled.div `
+    display: flex;
+    flex-direction: column;
+    `,
+
+    ContainerTituloEAno : styled.div `
+    background-color: #fff;
+    width: 38vw;
+    height: 35px;
+    border-radius: 2px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    `,
+
+    TituloFilme : styled.h4 `
+    font-family: Poppins, 'sans-serif';
+    font-size: 16px;
+    font-weight: 500;
+    text-align: left;
+    margin-left: 20px;
+    margin-right: auto;
+    `,
+
+    AnoFilme : styled.h6 `
+    font-family: Poppins, 'sans-serif';
+    font-size: 15px;
+    font-weight: 400;
+    text-align: right;
+    margin-right: 20px;
+    margin-left: auto;
     `
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
 import HomeStyle from '../assets/styles/Home/HomeStyle.js'
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const { ConteudoPrincipal, DefaultTitle, Input, Icon } = HomeStyle
+const { ConteudoPrincipal, DefaultTitle, Input, Icon, ContainerResponse, ImgFilme, ContainerMovieInfo, ContainerTituloEAno, TituloFilme, AnoFilme } = HomeStyle
 
 function Home() {
+    const apiKey = '3b60eddacb7025e1b48c11803ffc00a6';
+    const baseUrl = 'https://api.themoviedb.org/3/movie/popular';
+    const imageUrlBase = 'https://image.tmdb.org/t/p/w500';
+    let [data, setData] = useState({})
+
+    useEffect(() => {
+        axios.get(`${baseUrl}?api_key=${apiKey}`, {
+            params: {
+                api_key: apiKey,
+            },
+        })
+            .then(response => {
+                console.log(response.data);
+                setData(response.data)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, [])
+
+    function takeYear(date) {
+        return date ? date.substring(0, 4) : '';
+    }
+
     return (
         <>
             <ConteudoPrincipal>
@@ -14,6 +40,20 @@ function Home() {
                         placeholder='Pesquise algum filme aqui!' />
                     <Icon icon={faSearch} />
                 </div>
+
+                {data.results && data.results.map(movie => (
+                    <ContainerResponse>
+                        <ImgFilme key={movie.id} src={`${imageUrlBase}/${movie.poster_path}`} />
+                        <ContainerMovieInfo>
+                            <ContainerTituloEAno>
+                                <TituloFilme>{movie.original_title}</TituloFilme>
+                                <AnoFilme>{takeYear(movie.release_date)}</AnoFilme>
+                            </ContainerTituloEAno>
+
+                        </ContainerMovieInfo>
+                    </ContainerResponse>
+                ))}
+
             </ConteudoPrincipal>
         </>
     )
